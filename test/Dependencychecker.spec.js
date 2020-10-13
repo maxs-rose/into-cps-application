ef98ad9da0c9a999f8eb76d1a221e1d4634ed7b3
idhugoid <hugodsmacedo@gmail.com>
2020-10-13 15:27:01 +0200

    Initial idea for Unit tests

---
+++ test/Dependencychecker.spec.js
@@ -0,0 +1,67 @@
+const Application = require('spectron').Application
+const assert = require('assert')
+const expect = require('chai').expect;
+const electronPath = require('electron') // Require Electron from the binaries included in node_modules.
+const path = require('path')
+
+
+describe('The Dependency Checker', function () {
+  this.timeout(100000)
+
+
+
+  beforeEach(function () {
+    this.app = new Application({
+      path: electronPath,
+      args: [path.join(__dirname, '..')]
+    })
+
+    return this.app.start().then(app => {return app.client.waitUntilWindowLoaded()})
+  })
+
+  afterEach(function () {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop()
+    }
+
+    //server.stop(done);
+  })
+
+  xit('shows an initial window', function () {
+    this.app.client.waitUntilWindowLoaded().then( app => {app.webContents.isLoading().then(function (visible) {
+      console.log('window is loading? ' + visible)
+    })
+    
+  })
+  
+  })
+
+  it('should execute javascript', function () {
+    return this.app.client.waitUntilWindowLoaded().then( app => {
+      this.app.webContents.executeJavaScript('1 + 2')
+      .then(function (result) {
+        console.log(result) // prints 3
+        })
+    }) 
+
+  })
+
+  xit('should get all webcontents', function () {
+    this.app.client.waitUntilWindowLoaded().then( app => {
+      result = this.app.webContents.getAllWebContents()
+      console.log(result) // prints 3
+        
+    }) 
+
+  })
+
+  it('should work', async function () {
+    const { remote } = require('electron');
+    const mainProcess = remote.require('./main.js');
+  });
+  
+
+})
+
+
+
